62ac69ae0bc07d1479097b93ad374fefd0b46d78
TheSCodex <eliud1102@outlook.es>
2024-02-24 10:34:09 -0500

    Agregando la Tipografía del proyecto

--- SGE/tailwind.config.js
+++ SGE/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                manjari: ["Manjari", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
+                kanit: ["Kanit", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
