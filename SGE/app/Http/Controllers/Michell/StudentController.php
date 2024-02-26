8fc998a9d7d1b2f4716775375ec7a9623e5ef57c
mezaaziel06 <mezzaaazielle@gmail.com>
2024-02-26 13:12:52 -0600

    controllers

---
+++ SGE/app/Http/Controllers/Michell/StudentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Michell;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EstudiantesController extends Controller
+{
+    //
+}
