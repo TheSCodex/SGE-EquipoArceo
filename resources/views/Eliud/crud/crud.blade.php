26b0f4518cb8311a6aa00d1bf15557053620b0bc
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2024-02-27 08:54:26 -0500

    Imagenes

---
+++ resources/views/Eliud/crud/crud.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.directorsAssistantTemplate')
+@section('titulo')
+    CRUD
+@endsection
+@section('contenido')
+    <div>
+        <h1>Empiecen a chambear aquí</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Eliud\Documentos\DocumentsController;
+use App\Http\Controllers\Eliud\Reportes\ReportsController;
+use App\Http\Controllers\Elizabeth\carrerasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//RUTAS PARA REPORTES
+Route::get('reportes/director', [ReportsController::class, 'directorIndex']);
+Route::get('reportes/asistente', [ReportsController::class, 'assistantIndex']);
+
+Route::resource('reportes', ReportsController::class);
+
+//RUTAS PARA CRUD - DOCUMENTOS
+Route::resource('documentos', DocumentsController::class);
+
+//RUTAS PARA EL CRUD DE CARRERAS Y DIVISIONES
+Route::resource('carreras', carrerasController::class);
+
+
