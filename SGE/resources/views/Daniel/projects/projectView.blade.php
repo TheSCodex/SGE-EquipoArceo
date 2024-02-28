9de8e1e7b301f494c6e65137c78d21d6516b8f5e
Octavio Cruz <dorersempai@gmil.com>
2024-02-27 22:32:06 -0500

    aun mas responsive

--- SGE/resources/views/Daniel/projects/projectView.blade.php
+++ SGE/resources/views/Daniel/projects/projectView.blade.php
@@ -11,8 +11,8 @@
             <h1 class="text-2xl font-bold text-green-500 mb-[4%] sm:mb-[3%] border-b py-[1%] px-[1%] border-slate-700  ">Mi proyecto </h1>
         <div class="w-[91w] sm:w-[85vw] h-[80vh] flex flex-wrap sm:justify-between">
 
-            <div class="w-full sm:w-[68%] h-[25%] sm:h-full flex flex-wrap flex-col justify-between">
-                <div class="w-full bg-white px-[2%] py-[.5%] flex-col rounded-sm font-semibold sm:font-bold ">
+            <div class="w-full sm:w-[68%] h-fit  sm:h-full flex flex-wrap flex-col justify-between">
+                <div class="w-full bg-white px-[2%] sm:py-[.5%] flex-col rounded-sm font-semibold sm:font-bold my-[1%] sm:my-0">
                     <h3>Nombre del proyecto</h3>
                 </div>
 
@@ -24,12 +24,12 @@
 
 
 
-            <div class="sm:w-[30%] h-[82%] sm:h-full flex flex-wrap sm:flex-col justify-between mt-[1%] sm:my-0">
-                <div class="w-full bg-white px-[2%] py-[1.6%] rounded-sm font-semibold sm:font-bold text-sm mb-[.5%] sm:mb-0">
+            <div class="sm:w-[30%] h-[82%] sm:h-full flex flex-wrap sm:flex-col justify-between mt-[1%] sm:mt-0">
+                <div class="w-full bg-white px-[2%] py-[1.6%] rounded-sm font-semibold sm:font-bold text-sm mb-[.5%] mt-[2%] sm:mt-0 sm:mb-0">
                     <h3>Estado del proyecto</h3>
                 </div>
 
-                <div class=" w-full bg-white px-[2%] py-[.8%] rounded-sm font-semibold h-[14%] text-black text-opacity-[50%] flex flex-wrap justify-center items-center ">
+                <div class=" w-full h-fit my-[1%] sm:m-0 bg-white px-[2%] py-[.8%] rounded-sm font-semibold sm:h-[14%] text-black text-opacity-[50%] flex flex-wrap justify-center items-center ">
                     <div class="w-[80%] flex flex-wrap items-center h-full gap-[10%] ">
                         <img src="{{ asset('img/iconosDaniel/estado.svg') }}" class="w-[15%]" />
                         <p class="w-[70%]">Tu proyecto esta guardado como borrador</p>
