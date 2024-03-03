67a06cb98345103a5a77039094241ff224456166
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2024-03-03 01:24:21 -0500

    dos cruds

--- resources/views/Elizabeth/companies.blade.php
+++ resources/views/Elizabeth/companies.blade.php
@@ -15,20 +15,20 @@
     @endphp
     <main class="min-h-screen h-full">
         <div class="py-8 sm:px-20">
-            <div class="border-b border-gray-200 pb-2 flex flex-row max-sm:flex-col items-center">
-                <div class="sm:w-4/6">
+            <div class="border-b border-gray-200 pb-2 flex  flex-row max-sm:flex-col items-center">
+                <div class="md:w-4/6">
                     <h1 class="font-bold text-2xl font-roboto">Empresas</h1>
                 </div>
 
                     <div class=" flex justify-center ">
-                        <div class="flex flex-row border border-primaryColor p-1 items-center rounded-md">
+                        <div class="flex flex-row border border-primaryColor sm:p-1 items-center rounded-md">
                             <input id='search' placeholder="Buscador" type="text" class="placeholder-primaryColor  max-sm:w-20  focus:outline-none font-roboto">
                             <label for="search" class="items-center"><img src="/img/logos/search.svg"></label>
                         </div>
                         <div class="p-1 flex flex-x-0 flex-col">
                             <button><img src="/img/logos/caret.svg"></button>
                         </div>
-                        <div class="w-52">
+                        <div class="md:w-[15vw]">
                             <button class="bg-primaryColor text-white p-2 rounded-md font-semibold ">Agregar nueva empresa</button>
                         </div>
                     </div>
@@ -36,7 +36,7 @@
                     
 
             </div>
-            <div class="mt-6 w-full flex items-center justify-between">
+            <div class="sm:mt-[2%] w-full flex items-center justify-between">
                 <table class="w-full text-center">
                     <tr>
                         <th class="text-[#ACACAC] font-roboto text-xs">Nombre</th>
@@ -50,16 +50,16 @@
                     </tr>
                     @foreach($usuarios as $index => $usuario)
                     <tr>
-                        <td class="font-roboto font-bold py-5">{{ $usuario['nombre'] }}</td>
-                        <td class="font-roboto font-bold max-sm:hidden py-5">{{ $usuario['correo'] }}</td>
-                        <td class="font-roboto font-bold max-sm:hidden py-5">{{ $usuario['apellido'] }}</td>
-                        <td class="font-roboto font-bold max-sm:hidden py-5">{{ $usuario['rol'] }}</td>
-                        <td class="font-roboto font-bold max-sm:hidden py-5">{{ $usuario['nomina'] }}</td>
-                        <td class="font-roboto font-bold max-sm:hidden py-5">{{ $usuario['especialidad'] }}</td>
+                        <td class="font-roboto font-bold py-[2.5%]">{{ $usuario['nombre'] }}</td>
+                        <td class="font-roboto font-bold max-sm:hidden py-[2.5%]">{{ $usuario['correo'] }}</td>
+                        <td class="font-roboto font-bold max-sm:hidden py-[2.5%]">{{ $usuario['apellido'] }}</td>
+                        <td class="font-roboto font-bold max-sm:hidden py-[2.5%]">{{ $usuario['rol'] }}</td>
+                        <td class="font-roboto font-bold max-sm:hidden py-[2.5%]">{{ $usuario['nomina'] }}</td>
+                        <td class="font-roboto font-bold max-sm:hidden py-[2.5%]">{{ $usuario['especialidad'] }}</td>
                         <td class="font-roboto font-bold  py-5">
                             <div class=" flex  items-center justify-center gap-5">
-                                <button class="font-roboto font-bold py-5"><img src="/img/logos/pencil.svg"></button>
-                                <button class="font-roboto font-bold py-5"><img src="/img/logos/trash.svg"></button>
+                                <button class="font-roboto font-bold py-[2.5%]"><img src="/img/logos/pencil.svg"></button>
+                                <button class="font-roboto font-bold py-[2.5%]"><img src="/img/logos/trash.svg"></button>
                                 <button id="openModalBtn{{ $index }}" class="openModalBtn">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1DAF90" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -78,16 +78,16 @@
 
     @foreach($usuarios as $index => $usuario)
     <div id="myModal{{ $index }}" class="myModal hidden fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50">
-        <div class="modal my-8 p-6  bg-white w-[80vw] mx-auto h-[90vh] rounded-lg shadow-lg ">
-            <h2 class="text-4xl font-extrabold text-center mb-3">Detalles de la empresa</h2>
+        <div class="modal my-[4%] p-[3%]  bg-white  w-[74vw] mx-auto h-[85vh] rounded-lg shadow-lg ">
+            <h2 class="text-2xl  font-extrabold text-center mb-3">Detalles de la empresa</h2>
 
             <div class="border-b-2 border-t-2 items-center flex flex-row justify-between">
                 <p class="text-[#ACACAC] font-roboto">Correo Eletectrónico</p>
-                <p class="font-roboto font-bold py-5">{{ $usuario['correo'] }}</p>
+                <p class="font-roboto font-bold py-[2.5%]">{{ $usuario['correo'] }}</p>
             </div>
             <div class="border-b-2 border-t-2 items-center flex flex-row justify-between">
                 <p class="text-[#ACACAC] font-roboto">Celular</p>
-                <p class="font-roboto font-bold py-5">{{ $usuario['apellido'] }}</p>
+                <p class="font-roboto font-bold py-[2.5%]">{{ $usuario['apellido'] }}</p>
             </div>
             <div class="border-b-2 border-t-2 items-center flex flex-row justify-between">
                 <p class="text-[#ACACAC] font-roboto">Registro</p>
@@ -101,9 +101,10 @@
                 <p class="text-[#ACACAC] font-roboto">Sector comercial</p>
                 <p class="font-roboto font-bold py-5">{{ $usuario['especialidad'] }}</p>
             </div>
-            <button class="close bg-red rounded-lg px-2 py-4 mt-6 w-full text-white">Cerrar Modal</button>
+            <button class="close bg-red rounded-lg md:px-2 lg:py-4 md:mt-6 w-full text-white">Cerrar Modal</button>
         </div>
     </div>
+
     @endforeach
 
     <script>
