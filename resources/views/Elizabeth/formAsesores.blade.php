c6f46756a375c670f1e5733be74b2b1605733587
Can <ethanbarea@gmail.com>
2024-03-03 22:49:30 -0500

    form realizado

--- resources/views/Elizabeth/formAsesores.blade.php
+++ resources/views/Elizabeth/formAsesores.blade.php
@@ -1,43 +1,43 @@
 @extends('templates.administratorTemplate')
 @section('contenido')
-<div class="w-full h-screen flex justify-center items-center bg-white">
-    <form action="{{url('user')}}" method="POST" class="flex flex-col font-montserrat space-y-5 w-[500px]">
-        <div class="w-full h-fit flex justify-start">
-            <h1 class="text-3xl font-bold">Añadir asesor</h1>
+<div class="w-full md:h-full xl:h-screen flex justify-center xl:items-center bg-white">
+    <form action="{{url('panel-empresas.store')}}" method="POST" class="flex flex-col font-montserrat mx-10 space-y-5 lg:w-[40vw] sm:w-[60vw]">
+        <div class="w-full h-fit flex justify-center mt-4">
+            <h1 class="text-3xl font-bold">Añadir Asesores</h1>
             @csrf
         </div>
         <div class="w-full h-fit flex flex-col space-y-2">
             <div class="w-full space-y-2">
                 <p class="text-sm">Nombre</p>
-                <input type="text" name="name_user" value="{{ old('name_user') }}" class="text-sm w-full rounded-md border-lightGray border-2 px-4 py-3" placeholder="Nombre">
-                @error('name_user')
+                <input type="text" name="" value="{{ old('') }}" class="text-sm  w-full rounded-md border-lightGray border-2 px-[2%] py-[3%]" placeholder="Nombre">
+                @error('')
                     <p class="text-[#ff0000] text-sm">
                         {{ $message }}
                     </p>
                 @enderror
             </div>
             <div class="w-full space-y-2">
-                <p class="text-sm space-y-2">Correo</p>
-                <input type="text" name="email_user" value="{{old('email_user')}}" class="text-sm w-full rounded-md border-lightGray border-2 px-4 py-3" placeholder="Correo">
-                @error('email_user')
+                <p class="text-sm">Correo Electronico</p>
+                <input type="text" name="" value="{{old('')}}" class="text-sm  w-full rounded-md border-lightGray border-2 px-[2%] py-[3%]" placeholder="nombre@ejemplo.com">
+                @error('')
+                    <p class="text-[#ff0000] text-sm">
+                        {{ $message }}
+                    </p>
+                @enderror
+            </div>
+            <div class="w-full space-y-2">
+                <p class="text-sm space-y-2">Numero Celular</p>
+                <input type="text" name="" value="{{old('')}}" class="text-sm  w-full rounded-md border-lightGray border-2 px-[2%] py-[3%]" placeholder="Numero celular">
+                @error('')
                 <p class="text-[#ff0000] text-sm">
                     {{ $message }}
                 </p>
                 @enderror
             </div>
             <div class="w-full space-y-2">
-                <p class="text-sm">Celular</p>
-                <input type="text" name="role_user" value="{{old('role_user')}}" class="text-sm w-full rounded-md border-lightGray border-2 px-4 py-3" placeholder="Celular">
-                @error('role_user')
-                    <p class="text-[#ff0000] text-sm">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-            <div class="w-full space-y-2">
                 <p class="text-sm">Grado academico</p>
-                <input type="text" name="id_user" value="{{old('id_user')}}" class="text-sm w-full rounded-md border-lightGray border-2 px-4 py-3" placeholder="Grado academico">
-                @error('id_user')
+                <input type="text" name="" value="{{old('')}}" class="text-sm  w-full rounded-md border-lightGray border-2 px-[2%] py-[3%]" placeholder="Grado academico">
+                @error('')
                     <p class="text-[#ff0000] text-sm">
                         {{ $message }}
                     </p>
@@ -45,15 +45,14 @@
             </div>
             <div class="w-full space-y-2">
                 <p class="text-sm">Departamento</p>
-                <input type="text" name="field_user" value="{{old('field_user')}}" class="text-sm w-full rounded-md border-lightGray border-2 px-4 py-3" placeholder="Departamento">
-                @error('field_user')
+                <input type="text" name="" value="{{old('')}}" class="text-sm  w-full rounded-md border-lightGray border-2 px-[2%] py-[3%]" placeholder="Departamento">
+                @error('')
                     <p class="text-[#ff0000] text-sm">
                         {{ $message }}
                     </p>
                 @enderror
-            </div>
         </div>
-        <button type="submit" class="p-2 bg-primaryColor w-[500px] rounded-md text-white">Añadir asesor</button>
+        <button type="submit" class="p-[1%] bg-primaryColor lg:w-[40vw] rounded-md text-white">Añadir asesor</button>
     </form>
 </div>
 @endsection('contenido')
