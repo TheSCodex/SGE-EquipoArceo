fa1115b3eb5d8b8201f8d9b5577820085840fe22
kevin-alexis <kevinlevin367@outlook.com>
2024-03-06 08:42:36 -0500

    Fixed Bug

--- SGE/routes/web.php
+++ SGE/routes/web.php
@@ -41,7 +41,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('login');
 });
 
 // Rutas para Reportes
