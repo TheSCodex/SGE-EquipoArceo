95f2f55eb1f273f487a8ffe97e0fd457b999ce1a
Alexis Dolores <alexisdanieldr@gmail.com>
2024-03-07 19:09:59 -0500

    Antes del pull

--- SGE/resources/views/Daniel/asesor/DashboardAdvisor.blade.php
+++ SGE/resources/views/Daniel/asesor/DashboardAdvisor.blade.php
@@ -7,11 +7,7 @@
 @section('contenido')
     <section class="flex flex-col justify-center items-center bg-[#F3F5F9]">
         <div class="sm:p-8 text-left w-[90%]">
-            <h1 class="text-2xl font-bold text-green-500 mb-[4%] sm:mb-[3%] border-b py-[1%] px-[1%] border-slate-700  ">
-                Bienvenido, Asesor Academico </h1>
-            <div class="w-[91w] sm:w-[85vw] h-[80vh] flex flex-wrap sm:justify-between">
-                <h1>Que show</h1>
-            </div>
+            <h1>Que show</h1>
         </div>
     </section>
 @endsection
