8323a737e8ac426d18b9b45196e0ef1975afa5be
Alexander <alexdroide466@gmail.com>
2024-03-10 13:03:43 -0500

    Arreglos

--- SGE/resources/views/templates/directorsAssistantTemplate.blade.php
+++ SGE/resources/views/templates/directorsAssistantTemplate.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,30 +8,31 @@
     @vite('resources/css/app.css')
 
 </head>
+
 <body>
-    <header class="p-1 md:p-5 grid grid-cols-3 gap-10 border-b-[1px] border-secondaryColor">
+    <header class="p-1 md:p-[1rem] md:flex justify-around items-center border-b-[1px] border-secondaryColor">
         <a href="#" class=" text-center flex justify-center">
-            <img src="/img/logos/logo-utCancún.png" class="w-28" alt="">
+            <img src="/img/logos/logo-utCancún.png" class="w-20 h-7" alt="Logo-ut">
         </a>
-    
-        <ul class="hidden md:flex gap-6 justify-center items-center">
+
+        <ul class="hidden md:flex md:gap-10 justify-center font-roboto items-center text-sm">
             <li>
-                <a href="#">Estudiantes</a>
+                <a href="#" class="hover:border-b-2 hover:border-primaryColor">Estudiantes</a>
             </li>
             <li>
-                <a href="#">Proyectos</a>
+                <a href="#" class="hover:border-b-2 hover:border-primaryColor">Proyectos</a>
             </li>
             <li>
-                <a href="#">Reportes</a>
+                <a href="#" class="hover:border-b-2 hover:border-primaryColor">Reportes</a>
             </li>
             <li>
-                <a href="#">Cartas</a>
+                <a href="#" class="hover:border-b-2 hover:border-primaryColor">Cartas</a>
             </li>
             <li>
-                <a href="/books">Libros</a>
+                <a href="/books" class="hover:border-b-2 hover:border-primaryColor">Libros</a>
             </li>
         </ul>
-    
+
         <ul class="hidden md:flex gap-6 justify-center">
             <button
                 class="flex justify-center items-center px-4 p-2 transition duration-300 ease-in-out rounded-full text-red-600 font-light text-white bg-[#999999]">
@@ -50,5 +52,5 @@
     @yield('scripts-book')
 
 </body>
-</html>
 
+</html>
