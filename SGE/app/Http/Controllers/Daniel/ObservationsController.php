a9ca2a195dc73889f273a5b1e06e8f839899c053
Toto-Sempai <totochucl@gmail.com>
2024-03-12 00:51:50 -0500

    avances pa que vean que si chambea

--- SGE/app/Http/Controllers/Daniel/ObservationsController.php
+++ SGE/app/Http/Controllers/Daniel/ObservationsController.php
@@ -10,62 +10,53 @@ use Illuminate\Support\Facades\Auth;
 
 class ObservationsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param int $projectId
-     * @return \Illuminate\Contracts\View\View
-     */
     public function index($projectId)
     {
         $intern = Intern::where('user_id', Auth::id())
-            ->where('project_id', $projectId)
+            ->where('project_id', $projectId) //por si hacen lo del id por url
             ->first();
 
-        $tutorComment = Comment::where('academic_advisor_id', $intern->academic_advisor_id)
+        $tutorComment = Comment::where('academic_advisor_id', $intern->academic_advisor_id) //id del tutor jeje
             ->first();
 
         $otherComments = Comment::where('project_id', $projectId)
             ->where('academic_advisor_id', '<>', $intern->academic_advisor_id)
             ->get();
 
-        return view('Daniel.Observation', compact('tutorComment', 'otherComments'));
+        return view('Daniel.Observation', compact('tutorComment', 'otherComments'));// los comentarios x (ya dejenme dormir)
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        // Puedes implementar esto si es necesario
+        // Implementar si es necesario.
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        // Puedes implementar esto si es necesario
+        // Implementar si es necesario.
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        // Puedes implementar esto si es necesario
+        // Implementar si es necesario.
     }
 
-    // Resto de las funciones...
+    public function edit(string $id)
+    {
+        // Implementar si es necesario.
+    }
+
+    public function update(Request $request, string $id)
+    {
+        // Implementar si es necesario.
+    }
+
+    public function destroy(string $id)
+    {
+        // Implementar si es necesario.
+    }
 }
+//por si no jala jajaja
 /*
 <?php
 
