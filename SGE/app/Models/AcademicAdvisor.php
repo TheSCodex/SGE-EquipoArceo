0c6560d73b95f0c80b0a420dfc57f9f6b7b15fc3
Toto-Sempai <totochucl@gmail.com>
2024-03-15 20:59:22 -0500

    De los cambios de octavio me manda conflicto asi que lo regrese

--- SGE/app/Models/AcademicAdvisor.php
+++ SGE/app/Models/AcademicAdvisor.php
@@ -26,12 +26,6 @@ class AcademicAdvisor extends Model
     {
         return $this->hasMany(ProjectAdvisorsLikes::class, 'id_academic_advisor');
     }
-
-    public function likedProjects()
-    {
-        return $this->hasMany(ProjectAdvisorsLikes::class, 'id_academic_advisor');
-    }
-
     
     public function event()
     {
