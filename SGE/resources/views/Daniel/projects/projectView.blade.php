d6ef0c783a691538ebe54077c050c70ee926abb3
Octavio Cruz <dorersempai@gmil.com>
2024-03-18 11:20:00 -0500

    Reparando detalles mayores esteticos en la view de anteproyecto del estudiante

--- SGE/resources/views/Daniel/projects/projectView.blade.php
+++ SGE/resources/views/Daniel/projects/projectView.blade.php
@@ -1,30 +1,28 @@
 @extends('./templates/studenTemplate')
 
 @section('titulo')
-    Bienvenido
+Bienvenido
 @endsection
 
 @section('contenido')
 
-    <section class="flex flex-col justify-center items-center bg-[#F3F5F9] min-h-full flex-grow">
-        <div class="sm:p-8 text-left w-[90%] mb-[2vh] sm:mb-0 ">
-            <h1 class="text-2xl font-bold text-green-500 mb-[4%] sm:mb-[3%] border-b py-[1%] px-[1%] border-slate-700  ">
-                Mi Anteproyecto
-            </h1>
+<section class="flex flex-col justify-center items-center bg-[#F3F5F9] min-h-full flex-grow">
+    <div class="sm:p-8 text-left w-[90%] mb-[2vh] sm:mb-0 ">
+        <h1 class="text-2xl font-bold text-green-500 mb-[4%] sm:mb-[3%] border-b py-[1%] px-[1%] border-slate-700  ">
+            Mi Anteproyecto 
+        </h1>
 
-            <div class="w-[91w] sm:w-[85vw] sm:min-h-[78vh] items-center flex flex-wrap sm:justify-between flex-grow ">
+        <div class="w-[91w] sm:w-[85vw] sm:min-h-[78vh] items-center flex flex-wrap sm:justify-between flex-grow ">
 
+            <div
+                class="w-full sm:w-[68%] min-h-[50vh] sm:h-full flex flex-wrap lg-flex-col justify-between gap-[.5vh] md:gap-[1vh]">
                 <div
-
                     class="w-full bg-white px-[2%] sm:py-[.5%] flex-col rounded-sm font-semibold sm:font-bold my-[1%] sm:my-0">
                     <h3>Nombre del proyecto:     @if(isset($project))   <span class="mx-[1%] font-semibold">{{ $project->name }}</span>    @endif</h3>
                 </div>
 
-
-                    <div
-                        class="w-full min-h-[92.5%] bg-white px-[2%] py-[.5%] rounded-sm font-bold flex flex-wrap items-center flex-col">
-                        <div class="w-full px-[3%] self-start">
-
+                <div class="w-full min-h-[92.5%] bg-white px-[2%] py-[.5%] rounded-sm font-bold flex flex-wrap items-center flex-col">
+                    <div class="w-full px-[3%] self-start">
 
                         @if(isset($project))
                         <p class=" w-[80%] sm:w-[full] text-lg sm:text-lg">Nombre de la empresa: <span class="mx-[1%] font-semibold">{{ $company->name ?? 'No disponible'}}</span></p>
@@ -34,7 +32,6 @@
                     <div class="flex flex-wrap flex-col flex-grow items-center justify-evenly min-h-[57vh] mt-[1.5%] gap-[10] w-full"> <!-- Esta linea es salida de los confines del inframundo -->
                         <div class="w-full flex flex-wrap justify-center">
                             <p class=" w-[80%] sm:w-[80%] text-lg sm:text-lg text-center">Área de desempeño</p>
-
                                 <p class=" w-[80%] sm:w-[90%] font-normal text-sm    text-center">
                                     {{ $project->description }}
                                 </p>
@@ -59,27 +56,27 @@
                                 <p class=" w-[80%] sm:w-[90%] font-normal text-sm    text-center">
                                     {{ $project->activities_to_do }}
                                 </p>
-
+                                
                             </div>
                             <a href="{{ route('editAnteproyecto', $project->id) }}" class="self-end px-[2vw] bg-[#02AB82] text-white rounded-md">Editar</a>
                         </div>
 
-                    @else
-                        <div
-                            class="flex flex-wrap flex-col flex-grow items-center justify-center sm:min-h-[70vh] mt-[1.5%] gap-[10] ">
-                            <!-- Esta linea es salida de los confines del inframundo -->
+                        @else
 
-                            <p class=" w-[80%] sm:w-[38%] text-lg sm:text-2xl text-center ">Aun no tienes un
-                                Anteproyecto. Empieza a trabajarlo ahora</p>
+                    <div class="flex flex-wrap flex-col flex-grow items-center justify-center sm:min-h-[70vh] mt-[1.5%] gap-[10] "> <!-- Esta linea es salida de los confines del inframundo -->
+
+                        <p class=" w-[80%] sm:w-[38%] text-lg sm:text-2xl text-center ">Aun no tienes un
+                            Anteproyecto. Empieza a trabajarlo ahora</p>
                             <a href="{{ route('formanteproyecto') }}"
-                                class="block bg-[#02AB82] text-white rounded-md px-[2%] py-[1%] m-[2%] font-normal  text-center text-sm">Crea
-                                uno ahora</a>
+                            class="block bg-[#02AB82] text-white rounded-md px-[2%] py-[1%] m-[2%] font-normal  text-center text-sm">Crea
+                            uno ahora</a>
+                            </div>
                         </div>
+                            
+                        @endif
                     </div>
-                    @endif
                 </div>
-            </div>
-
+            
             <div
                 class="sm:w-[31%] h-[82%] sm:h-full flex flex-wrap sm:flex-col justify-between mt-[1%] sm:mt-0 self-center gap-[1vh]">
                 <div
@@ -101,11 +98,11 @@
                     <div class="w-[80%] flex flex-wrap items-center h-full gap-[10%]">
                         <img src="{{ asset('img/iconosDaniel/votos.svg') }}" class="w-[15%]" />
                         <div class=" flex justify-between flex-wrap flex-row-reverse">
-                            {{-- @if($project->like == 0)
+                        {{--    @if($project->like == 0)
                                 <p>Aun no tienes votos</p>
                             @else
                                 <p>Tienes {{$project->like}} voto(s)</p>
-                            @endif --}}
+                            @endif--}}
                         </div>
                     </div>
                 </div>
@@ -121,11 +118,9 @@
                             <p class=' text-black w-full font-normal text-sm'>Asesor</p>
                             <p class=' text-black w-full font-normal text-sm'>{{ $comment->content }}</p>
                         </div>
-
                         @endforeach
                         <a href="{{ route('observationsAnteproyecto') }}" class="bg-[#02AB82] text-sm text-white font-lg px-[.5vw] py-[.2vw] rounded-md my-[2%]">Ver observaciones</a>
                     </div>
-
                 @elseif(isset($comments) && count($comments) == 0)
                 <div class="w-full bg-white px-[10%] py-[.8%] rounded-sm font-bold h-[41.5vh]  flex justify-center items-center text-xl overflow-y-auto">
                     <p class=' text-center text-black opacity-[60%]'>No hay comentarios en tu anteproyecto.</p>
@@ -134,10 +129,9 @@
                     <div class="w-full bg-white px-[10%] py-[.8%] rounded-sm font-bold h-[41.5vh]  flex justify-center items-center text-xl overflow-y-auto">
                         <p class=' text-center text-black opacity-[60%]'>Aun no tienes un anteproyecto Empieza a trabajarlo ahora</p>
                     </div>
-
                 @endif
             </div>
         </div>
-        </div>
-    </section>
+    </div>
+</section>
 @endsection
