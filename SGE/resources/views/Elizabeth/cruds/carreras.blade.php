27661617b0f358b39c0d82d2ccf40f35ba01e0c6
Can <ethanbarea@gmail.com>
2024-03-18 15:47:55 -0500

    crud carreras mapeado

--- SGE/resources/views/Elizabeth/cruds/carreras.blade.php
+++ SGE/resources/views/Elizabeth/cruds/carreras.blade.php
@@ -1,19 +1,6 @@
 @extends('templates/authTemplate')
 @section('titulo', 'Panel de Carrera y academia')
 @section('contenido')
-    {{-- Test --}}
-    @php
-    $users = [
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-        ['Carrera' => 'Desarollo de software', 'División' => 'Software'],
-    ];
-    @endphp
     <style>
         table {
             border-collapse: separate;
@@ -61,11 +48,11 @@
             {{-- cards --}}
             <div class="lg:hidden w-full mb-5">
                 <div class="grid md:grid-cols-2 gap-4 w-full">
-                    @foreach ($users as $user)
+                    @foreach ($careers as $career)
                         <div class="bg-white rounded-lg shadow-md p-4 drop-shadow-2xl">
-                            <h2 class="text-lg font-bold">{{ $user['Carrera'] }}</h2>
+                            <h2 class="text-lg font-bold">{{ $career->name }}</h2>
 
-                            <p class="text-sm text-gray-500">División: {{ $user['División'] }}</p>
+                            <p class="text-sm text-gray-500">División: {{ $career->division->name }}</p>
       
                             <div class="flex justify-end mt-4">
                                    <a href="/editCareer">
@@ -88,10 +75,10 @@
                         <th class="text-[#ACACAC] font-roboto text-xs"></th>
                         <th class="text-[#ACACAC] font-roboto text-xs"></th>
                     </tr>
-                    @foreach ($users as $user)
+                    @foreach ($careers as $career)
                         <tr>
-                            <td class="font-roboto font-bold py-5">{{ $user['Carrera'] }}</td>
-                            <td class="font-roboto font-bold py-5">{{ $user['División'] }}</td>
+                            <td class="font-roboto font-bold py-5">{{ $career->name }}</td>
+                            <td class="font-roboto font-bold py-5">{{ $career->division->name }}</td>
                             <td class="font-roboto font-bold py-5">
                                 <a href="/editCareer"> <button><img src="/img/logos/pencil.svg"></button></a>
                             </td>
@@ -103,92 +90,5 @@
                 </table>
             </div>
         </div>
-<<<<<<< HEAD
-        <div class="hidden lg:block w-full">
-            <table class="text-center w-full">
-                <tr>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Carrera</th>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Division</th>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Academia</th>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Presidenta</th>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Editar</th>
-                    <th class="text-[#ACACAC] font-roboto text-xs">Eliminar</th>
-                </tr>
-                @foreach ($careers as $career)
-                <tr>
-                    <td class="font-roboto font-bold py-5">{{ $career->name }}</td>
-                    <td class="font-roboto font-bold py-5">{{ $career->division_name }}</td>
-                    <td class="font-roboto font-bold py-5">{{ $career->academy_name }}</td>
-                    <td class="font-roboto font-bold py-5">{{ $career->president_name}}</td>
-                    <td class="font-roboto font-bold py-5 cursor-pointer ">
-                        {{--
-                            <a href="{{ route('panel-careers.edit', $career->id_career) }}" class="flex justify-center">
-                            <img src="/img/logos/pencil.svg">
-                        </a>
-                        --}}
-                    </td>
-                    <td class="font-roboto font-bold py-5 cursor-pointer" onclick="confirmDelete('{{ $career->name }}', '{{ $career->id_career}}')">
-                        <form class="flex justify-center" id="deleteForm{{ $career->id}}" action="{{ route('panel-careers.destroy', $career->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <img src="/img/logos/trash.svg">
-                        </form>
-                        
-                    </td>
-                    
-                </tr>
-                @endforeach
-            </table>
-        </div>
-    </div>
-</div>
-    
-</section>
-
-<script>
-    function confirmDelete(careerName, careerId) {
-        Swal.fire({
-            title: '¿Estás seguro?',
-            text: `Estás a punto de eliminar a ${careerName}. Esta acción no se puede revertir.`,
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Sí, eliminarlo'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                document.getElementById('deleteForm' + careerId).submit();
-            }
-        });
-    }
-    function searchTable() {
-        var searchText = document.getElementById("search").value.toLowerCase();
-        var rows = document.querySelectorAll("table tr");
-        for (var i = 1; i < rows.length; i++) {
-            var row = rows[i];
-            var found = false;
-            for (var j = 0; j < row.cells.length; j++) {
-                var cell = row.cells[j];
-                if (cell.textContent.toLowerCase().indexOf(searchText) > -1) {
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                row.style.display = "";
-            } else {
-                row.style.display = "none";
-            }
-        }
-    }
-    
-        // Llamamos a la función searchTable() cuando se modifica el contenido del input de búsqueda
-        document.getElementById("search").addEventListener("input", searchTable);
-</script>
-
-
-
-=======
     </main>
->>>>>>> 3b92094501480732d33f3837606e24a369d504d5
 @endsection
