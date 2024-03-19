e73bfc7b54d5a213f02c2c8223a04ce329906e04
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2024-03-19 16:41:10 -0500

    Fix authentication when changing password for the first time

--- SGE/app/Http/Controllers/Pipa/ChangePasswordFirstTime.php
+++ SGE/app/Http/Controllers/Pipa/ChangePasswordFirstTime.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\Pipa\ChangePasswordRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
 
 class ChangePasswordFirstTime extends Controller
 {
@@ -17,20 +19,28 @@ class ChangePasswordFirstTime extends Controller
 
     public function store(ChangePasswordRequest $request)
     {
-        $user = Auth::user();
+
+        // Obtener el valor de 'user' desde la sesión
+        $dataUser = Session::get('user');
 
         // Validar que las contraseñas sean iguales
         if ($request->input('new_password') !== $request->input('confirmed_password')) {
             return redirect()->back()->withErrors(['new_password' => 'Las contraseñas no coinciden'])->withInput();
         }
 
-        if (Hash::check($request->input('new_password'), $user->password)) {
+        if (Hash::check($request->input('new_password'), $dataUser->password)) {
             return redirect()->back()->withErrors(['new_password' => 'La contraseña no puede ser igual a la predeterminada'])->withInput();
         }
 
         // Cambiar la contraseña del usuario
-        $user->update(['password' => Hash::make($request->input('new_password'))]);
+        $dataUser->update(['password' => Hash::make($request->input('new_password'))]);
 
-        return redirect('/');
+        // Autenticar al usuario después de cambiar la contraseña
+        Auth::login($dataUser);
+
+        // Regenerar la sesión
+        $request->session()->regenerate();
+
+        return redirect('/login');
     }
 }
