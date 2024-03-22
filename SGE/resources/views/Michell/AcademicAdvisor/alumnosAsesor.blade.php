189f394a6389343d03ffc55cb4a58fb65e540af0
mezaaziel06 <mezzaaazielle@gmail.com>
2024-03-22 10:59:40 -0600

    pagination new

--- SGE/resources/views/Michell/AcademicAdvisor/alumnosAsesor.blade.php
+++ SGE/resources/views/Michell/AcademicAdvisor/alumnosAsesor.blade.php
@@ -1,9 +1,12 @@
-@extends('templates/authTemplate')
-@section('contenido')
 
-    <div class="bg-white min-h-screen flex flex-col items-center px-0 lg:px-20">
-    <section class="w-full p-4 flex items-center mt-7">
-    <h1 class="font-bold font-montserrat text-xl mb-2 text-center md:text-right ">Estudiantes Asesorados</h1>
+@extends('templates/authTemplate')
+@section('titulo', 'Estuadiantes Asesorados')
+@section('contenido')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<section class="flex flex-col justify-center items-center  min-h-full flex-grow">
+    <div class="sm:p-8 text-left w-[90%] mb-[2vh] sm:mb-0 ">
+        <div class="border-b border-gray-200 mt-5 pb-2 mx-auto w-11/12 md:flex md:items-center md:justify-between">
+        <h1 class="font-bold font-montserrat text-xl mb-2 text-center md:text-left">Estudiantes Asesorados</h1>
         <div class="flex items-center flex-row justify-end">
             <div>
                 <div class="hidden md:flex items-center relative" >
@@ -19,13 +22,13 @@
                 <div class="flex items-center relative" >
                     <input id='searchMovil' class="border-primaryColor placeholder-primaryColor border-b border rounded-md w-full mb-2 sm:mb-0 " type="search" placeholder="Buscar...." style="color: green;">
                 </div>
-
+            </div>
+           
 
         </div>
     </div>
-    </section>
 
-    <section class="w-full px-2 lg:px-16">
+   <section class="w-full px-2 lg:px-16">
         <div class="mx-8 my-5">
             <table class="w-full min-w-[600px] divide-y divide-gray-200">
                 <thead>
@@ -70,12 +73,70 @@
                                 No hay estudiantes asesorados.
                             </td>
                         </tr>
+                       
                     @endif
                 </tbody>
             </table>
+            {{$interns->links()}}
         </div>
     </section>
 </div>
-@endsection
 
+<script>
+    function confirmDelete(advisorName, advisorId) {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: Estás a punto de eliminar a ${advisorName}. Esta acción no se puede revertir.,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sí, eliminarlo'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('deleteForm' + advisorId).submit();
+            }
+        });
+    }
+    function searchTable() {
+        var searchText = document.getElementById("search").value.toLowerCase();
+        var rows = document.querySelectorAll("table tr");
+        for (var i = 1; i < rows.length; i++) {
+            var row = rows[i];
+            var found = false;
+            for (var j = 0; j < row.cells.length; j++) {
+                var cell = row.cells[j];
+                if (cell.textContent.toLowerCase().indexOf(searchText) > -1) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                row.style.display = "";
+            } else {
+                row.style.display = "none";
+            }
+        }
+    }
+    
+        // Llamamos a la función searchTable() cuando se modifica el contenido del input de búsqueda
+        document.getElementById("search").addEventListener("input", searchTable);
+</script>
+
+
+<script>
+    function searchMobileTable() {
+        var searchText = document.getElementById("searchMovil").value.toLowerCase();
+        var advisors = document.querySelectorAll(".grid.md\\:grid-cols-2.gap-4.w-full > div");
+        
+        advisors.forEach(function(advisor) {
+            var advisorText = advisor.innerText.toLowerCase();
+            var found = advisorText.indexOf(searchText) > -1;
+            advisor.style.display = found ? "" : "none";
+        });
+    }
+
+    document.getElementById("searchMovil").addEventListener("input", searchMobileTable);
+</script>
+@endsection
 
