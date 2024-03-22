b3d6141cea5be97642abc7aa2fb8419ecb26d478
Israel Boquin <ip345622@gmail.com>
2024-03-22 13:39:09 -0500

    Ya se extrae el asesor academico

--- SGE/resources/views/Michell/StudentHome/studentHome.blade.php
+++ SGE/resources/views/Michell/StudentHome/studentHome.blade.php
@@ -132,7 +132,10 @@
                                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
                     </div>
-                    <p>{{ $advisor->advisor_name }}</p>
+                    @foreach ($empresarial as $item)
+                        <p>{{ $item->asesor_empresarial }}</p>
+                    @endforeach
+
                 </div>
             </div>
 
