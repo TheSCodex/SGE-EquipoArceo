e9dedc4d879552b98c11fe7fdaa349d9f8f6df60
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2024-04-13 15:45:49 -0500

    fix(student-dash): se agrego validacion de asistente empresarial

--- SGE/resources/views/Michell/StudentHome/studentHome.blade.php
+++ SGE/resources/views/Michell/StudentHome/studentHome.blade.php
@@ -123,17 +123,17 @@
 
             <div class="space-y-2">
                 <div class="flex font-roboto text-base items-center space-x-5 bg-white rounded-md py-2">
-                    <div class="bg-primaryColor rounded-full p-1 ml-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                            stroke="white" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                        </svg>
-                    </div>
-                    @if ($advisor)
+                    @if ($advisor->academicAdvisor)
+                        <div class="bg-primaryColor rounded-full p-1 ml-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                                stroke="white" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            </svg>
+                        </div>
                         <p>{{ $advisor->academicAdvisor->user->name }}</p>
                     @else
-                        <p>Sin asesor académico</p>
+                        <p class="text-[#888] w-full text-center">Sin asesor académico</p>
                     @endif
                 </div>
             </div>
@@ -185,7 +185,7 @@
                 </div>
 
             </div>
-            <div class="grid grid-cols-1   md:grid-cols-1 gap-3 h-[280px] ">
+            <div class="grid grid-cols-1  md:grid-cols-1 gap-3 h-[230px] ">
                 <div class="bg-white p-5 font-black flex flex-col justify-center h-600px">
                     @if ($penalty === null)
                         <p class="text-center mb-5">No has recibido ninguna amonestación hasta el momento, buen trabajo!
