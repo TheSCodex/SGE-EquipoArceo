8cb38ee94368484999ceee2f58d9b534ea6a4227
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2024-04-15 11:03:51 -0500

    modificacion del form de asesores empresariales

--- SGE/resources/views/Elizabeth/formAsesores.blade.php
+++ SGE/resources/views/Elizabeth/formAsesores.blade.php
@@ -2,7 +2,7 @@
 @extends('templates/authTemplate')
 @section('contenido')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<div class="w-full md:px-[7em] md:mt-[2em] h-screen flex bg-white">
+<div class="w-full md:px-[7em] md:mt-[2em] min-h-screen flex bg-white">
     <form action="{{ route('panel-advisors.store') }}" method="POST" class="flex flex-col font-montserrat space-y-5 w-full mt-4 md:mt-0 md:w-full">
     
         @method('POST')
@@ -23,8 +23,10 @@
             <p class="text-sm">Correo</p>
             <input type="text" name="email" value="{{ old('email') }}" class="text-sm rounded-md border-lightGray border-2 px-4 py-3 w-[20em] md:w-[35em]" placeholder="Correo">
             @error('email')
-                <p class="text-[#ff0000] text-sm">{{ $message }}</p>
-            @enderror
+            <p class="text-[#ff0000] text-sm max-w-[70vw]">
+                {{ $message }}
+            </p>
+                @enderror
         </div>
     </div> 
 </div>
